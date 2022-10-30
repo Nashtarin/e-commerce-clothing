fc894dfc33910ed42e914f1136e1526fd37a345c
Nashtarin Nur <nashtarin.nur@gmail.com>
2022-10-30 21:29:10 +0600

    styled componenet done

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBx6bnETIhJbQtXNzpOpg4qhlwCScifrwg",
+  authDomain: "e-commerce-clothing-63620.firebaseapp.com",
+  projectId: "e-commerce-clothing-63620",
+  storageBucket: "e-commerce-clothing-63620.appspot.com",
+  messagingSenderId: "771087475101",
+  appId: "1:771087475101:web:d0802e99fe051cb2a74f74"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
